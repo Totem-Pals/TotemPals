8decf76c0c55e970824e2e8e1bdc65916200fcd7
amigurumi-crypto <64930758+amigurumi-crypto@users.noreply.github.com>
2020-10-25 04:06:18 -0400

    spike signal changes

--- Levels/Spikes.gd
+++ Levels/Spikes.gd
@@ -1,12 +1,4 @@
 extends StaticBody2D
 
-func _ready():
-	pass # Replace with function body.
-
-
-func _process(delta):
-	
-	var bodies = $Area2D.get_overlapping_bodies()
-	if bodies:
-		print ("collide")
-	pass
+func _on_Area2D_body_entered(body):
+	print ("damage")
