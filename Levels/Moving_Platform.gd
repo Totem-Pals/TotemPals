b1fdda6bab2776c58337755264dfaf974bbb23aa
amigurumi-crypto <64930758+amigurumi-crypto@users.noreply.github.com>
2020-10-28 03:00:23 -0400

    new export variable for platform

--- Levels/Moving_Platform.gd
+++ Levels/Moving_Platform.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export var length = 1
 export var speed = 4
+export var x_endpoint = 160
 
 onready var child2D = $Node2D
 onready var tile = $Node2D/TileMap
@@ -9,19 +10,17 @@ onready var tween = $Node2D/Tween
 onready var tween2 = $Node2D/Tween2
 
 func _ready():
-	print (length)
 	for i in range(5 - length):
-		print(i)
 		tile.set_cellv(Vector2(4 - i, 0), -1)
 	tile.update_bitmask_region(Vector2(0,0), Vector2(5,0))
 	_on_Tween2_tween_all_completed()
 	
 func _on_Tween_tween_all_completed():
-	tween2.interpolate_property(child2D, "position", Vector2(160, 0), Vector2(0, 0), speed,
+	tween2.interpolate_property(child2D, "position", Vector2(x_endpoint, 0), Vector2(0, 0), speed,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween2.start()
 	
 func _on_Tween2_tween_all_completed():
-	tween.interpolate_property(child2D, "position", Vector2(0, 0), Vector2(160, 0), speed,
+	tween.interpolate_property(child2D, "position", Vector2(0, 0), Vector2(x_endpoint, 0), speed,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
