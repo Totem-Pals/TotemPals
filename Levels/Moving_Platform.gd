813c9fd13a5a3d3c47e309dcca9fcd93af0fc787
amigurumi-crypto <64930758+amigurumi-crypto@users.noreply.github.com>
2020-11-01 23:25:25 -0500

    testing tutorial level 1

--- Levels/Moving_Platform.gd
+++ Levels/Moving_Platform.gd
@@ -3,6 +3,7 @@ extends Node2D
 export var length = 1
 export var speed = 4
 export var x_endpoint = 160
+export var y_endpoint = 0
 
 onready var child2D = $Node2D
 onready var tile = $Node2D/TileMap
@@ -16,11 +17,11 @@ func _ready():
 	_on_Tween2_tween_all_completed()
 	
 func _on_Tween_tween_all_completed():
-	tween2.interpolate_property(child2D, "position", Vector2(x_endpoint, 0), Vector2(0, 0), speed,
+	tween2.interpolate_property(child2D, "position", Vector2(x_endpoint, y_endpoint), Vector2(0, 0), speed,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween2.start()
 	
 func _on_Tween2_tween_all_completed():
-	tween.interpolate_property(child2D, "position", Vector2(0, 0), Vector2(x_endpoint, 0), speed,
+	tween.interpolate_property(child2D, "position", Vector2(0, 0), Vector2(x_endpoint, y_endpoint), speed,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
--- Levels/Portal.gd
+++ Levels/Portal.gd
@@ -1,20 +1,22 @@
 extends Area2D
 
 export var Entry = true
+export var OneWay = true
 export var PartnerLoc = Vector2(0,0)
 
 func _ready():
 	pass
 
 func _on_Portal_body_entered(body):
-	#75 was the y rect size of the test main character
-	var portal_offset = body.currentRectSize.y - 75
+	#32 is the rect size of the current main character
+	var portal_offset = body.currentRectSize.y - 32
 	if Entry:
 		body.position = PartnerLoc - Vector2(0, portal_offset)
 
 func _on_Portal_body_exited(body):
-	if !Entry:
-		Entry = true
-	else:
-		Entry = false
+	if !OneWay:
+		if !Entry:
+			Entry = true
+		else:
+			Entry = false
 
