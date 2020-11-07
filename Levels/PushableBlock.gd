fe42f847e1663db415360326018a2a9a786cc07b
amigurumi-crypto <64930758+amigurumi-crypto@users.noreply.github.com>
2020-11-06 22:59:45 -0500

    checkpoint placement, and world final 4 started

--- Levels/PushableBlock.gd
+++ Levels/PushableBlock.gd
@@ -7,11 +7,18 @@ func _ready():
 func _on_Area2D_body_entered(body):
 
 	if(body.is_in_group("Player")):
-		body.check_ability("strong")
-		$RigidBody2D.set_deferred("mode", 0)
+		if !body.check_ability("strong"):
+			$RigidBody2D.set_deferred("mode", 1)
 
 
 
 func _on_Area2D_body_exited(body):
 	print("out")
+	$RigidBody2D.set_deferred("mode", 2)
+
+
+func _on_Area2D2_body_entered(body):
 	$RigidBody2D.set_deferred("mode", 1)
+
+func _on_Area2D2_body_exited(body):
+	$RigidBody2D.set_deferred("mode", 2)
