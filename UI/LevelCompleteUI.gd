cb7396910df21dd8ddd94e4a7c9120cc0a0d7149
Wyxaldir <vazoyer@gmail.com>
2020-11-13 23:54:46 -0500

    more bug fixes.

--- UI/LevelCompleteUI.gd
+++ UI/LevelCompleteUI.gd
@@ -13,7 +13,8 @@ func _on_BackToMenu_pressed():
 
 func _on_NextLevel_pressed():
 	for i in range(LevelManager.levels.size()):
-		if get_parent().filename == LevelManager.levels[i].resource_name:
+		
+		if get_parent().get_parent().filename == LevelManager.levels[i].resource_path:
 			if  i + 1 >= LevelManager.levels.size():
 				return
 			get_tree().change_scene_to(LevelManager.levels[i+1])
